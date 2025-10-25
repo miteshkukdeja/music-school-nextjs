54ce284a9e9521e3f7352fcb0a55c4d31ff03268
Mitesh Kukdeja <collab.mitesh@gmail.com>
2025-10-25 23:08:48 +0530

    ✨ feat: added upcoming webinar section with hover effect card

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { FeaturedCourses } from "@/src/components/FeaturedCourses";
 import HeroSection from "@/src/components/HeroSection";
 import MusicSchoolTestimonials from "@/src/components/TestimonialCards";
+import { UpcomingWebinars } from "@/src/components/UpcomingWebinars";
 import WhyChooseUs from "@/src/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
+    <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02] ">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
+      <UpcomingWebinars />
     </main>
   );
 }
